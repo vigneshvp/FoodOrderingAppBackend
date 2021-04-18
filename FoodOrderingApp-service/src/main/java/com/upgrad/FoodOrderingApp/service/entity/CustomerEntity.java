22258da82c56e54277e19ba4f694ccaf6396582c
vigneshvp <viki3110@gmail.com>
2021-04-18 08:49:55 +0530

    Created CustomerEntity

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerEntity.java
@@ -0,0 +1,130 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "CUSTOMER")
+public class CustomerEntity implements Serializable {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @NotNull
+    @Size(max = 200)
+    private String uuid;
+
+    @Column(name = "FIRSTNAME")
+    @NotNull
+    @Size(max = 30)
+    private String firstName;
+
+    @Column(name = "LASTNAME")
+    @NotNull
+    @Size(max = 30)
+    private String lastName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    @Column(name = "EMAIL", unique = true)
+    @NotNull
+    @Email
+    @Size(max = 50)
+    private String email;
+
+    @Column(name = "CONTACT_NUMBER")
+    @Size(max = 30)
+    private String contactNumber;
+
+    @Column(name = "PASSWORD")
+    @NotNull
+    @Size(max = 255)
+    private String password;
+
+    @Column(name = "SALT")
+    @NotNull
+    @Size(max = 200)
+    private String salt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CustomerEntity)) return false;
+        CustomerEntity that = (CustomerEntity) o;
+        return id.equals(that.id) && uuid.equals(that.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, uuid);
+    }
+}
