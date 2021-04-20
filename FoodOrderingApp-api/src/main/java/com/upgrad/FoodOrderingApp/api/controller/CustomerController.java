063d89aeddb986571874d510386555d48f37344b
vigneshvp <viki3110@gmail.com>
2021-04-20 09:26:03 +0530

    Created CustomerController with signup endpoint

---
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
@@ -0,0 +1,51 @@
+package com.upgrad.FoodOrderingApp.api.controller;
+
+import com.upgrad.FoodOrderingApp.api.model.SignupCustomerRequest;
+import com.upgrad.FoodOrderingApp.api.model.SignupCustomerResponse;
+import com.upgrad.FoodOrderingApp.service.businness.CustomerBusinessService;
+import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Base64;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    CustomerBusinessService customerBusinessService;
+
+    @Autowired
+    public CustomerController(CustomerBusinessService customerBusinessService) {
+        this.customerBusinessService = customerBusinessService;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Customer Successfully registered"),
+            @ApiResponse(code = 409, message = "Conflict - ContactNumber is already taken")
+    })
+    @RequestMapping(method = RequestMethod.POST, path = "/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignupCustomerResponse> signup(final SignupCustomerRequest signupUserRequest)
+            throws SignUpRestrictedException {
+
+        final CustomerEntity customerEntity = new CustomerEntity();
+        customerEntity.setUuid(UUID.randomUUID().toString());
+        customerEntity.setFirstName(signupUserRequest.getFirstName());
+        customerEntity.setLastName(signupUserRequest.getLastName());
+        customerEntity.setEmail(signupUserRequest.getEmailAddress());
+        customerEntity.setPassword(signupUserRequest.getPassword());
+        customerEntity.setContactNumber(signupUserRequest.getContactNumber());
+        final CustomerEntity createdCustomerEntity = customerBusinessService.createCustomer(customerEntity);
+        SignupCustomerResponse customerResponse = new SignupCustomerResponse().id(createdCustomerEntity.getUuid())
+                .status("CUSTOMER SUCCESSFULLY REGISTERED");
+        return new ResponseEntity<SignupCustomerResponse>(customerResponse, HttpStatus.CREATED);
+    }
+}
